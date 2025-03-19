40edc971dc19f53ded98bfbeac57c547ecd4dc2e
michael.buchanan.career <michael.buchanan.career@gmail.com>
2025-03-19 09:08:50 -0400

    Adding base Terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable PM_USER {
+    description = "Proxmox user variable"
+    type        = string
+}
+
+variable PM_PASS {
+    description = "Proxmox password variable"
+    type        = string
+}
